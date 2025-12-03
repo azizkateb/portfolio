9f507acb3a70ee59fc10c79eba41adf9b110bc9f
azizkateb <azizkateb69@gmail.com>
2025-12-04 00:59:11 +0100

    .

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 
 @Component({
   selector: 'app-formulaire',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule,FormsModule],
   templateUrl: './formulaire.component.html',
   styleUrls: ['./formulaire.component.css']
 })
@@ -32,4 +33,5 @@ export class FormulaireComponent implements OnInit {
 
     this.form.reset();
   }
+  text = '';
 }
